e317cfae44b48fd039e93454f70e80c0fe03a9bf
Cristhian Monteiro <cristhiansousa8@gmail.com>
2023-05-17 20:25:15 -0300

    Inserindo candidatos, notas e a entrada do usuario
    Definindo a lista de candidatos e respectivas notas.
    Inserindo a Entrada do usuário (input) com as notas desejadas.

---
+++ resilia_pi_m2_selecao_candidatos_entrevista.py
@@ -0,0 +1,11 @@
+nota_entrevista = int(input("Digite a nota mínima (de 0 a 9) desejada na entrevista: "))
+nota_teorico = int(input("Digite a nota mínima (de 0 a 9) desejada no teste teórico: "))
+nota_pratica = int(input("Digite a nota mínima (de 0 a 9) desejada no teste prático: "))
+nota_soft = int(input("Digite a nota mínima (de 0 a 9) desejada na avaliação de soft skills: "))
+
+resultados = {'Freddie Mercury': 'e8_t9_p8_s8',
+         'Roger Waters': 'e8_t8_p7_s7',
+         'Kurt Cobain': 'e5_t7_p6_s3',
+         'Paul McCartney': 'e8_t7_p7_s8',
+         'Klaus Meine': 'e9_t8_p9_s8'
+}
