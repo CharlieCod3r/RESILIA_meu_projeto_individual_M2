22941f03dc877c371184180dc87bc57c4f6a620e
Cristhian Monteiro <cristhiansousa8@gmail.com>
2023-05-17 23:02:04 -0300

    Função para buscar candidatos
    definida a função que busca candidatos e faz a checagem devolvendo a lista de candidatos que são compatíveis com as notas informadas.

--- resilia_pi_m2_selecao_candidatos_entrevista.py
+++ resilia_pi_m2_selecao_candidatos_entrevista.py
@@ -1,3 +1,13 @@
+
+def buscar_candidatos(resultados, nota_entrevista, nota_teorico, nota_pratica, nota_soft):
+    candidatos = []
+    for candidato, notas in resultados.items():
+        nota_e, nota_t, nota_p, nota_s = [int(n[1]) for n in notas.split("_")]
+        if nota_e >= nota_entrevista and nota_t >= nota_teorico and nota_p >= nota_pratica and nota_s >= nota_soft:
+            candidatos.append(candidato)
+
+        return candidatos
+
 nota_entrevista = int(input("Digite a nota mínima (de 0 a 9) desejada na entrevista: "))
 nota_teorico = int(input("Digite a nota mínima (de 0 a 9) desejada no teste teórico: "))
 nota_pratica = int(input("Digite a nota mínima (de 0 a 9) desejada no teste prático: "))
@@ -9,3 +19,4 @@ resultados = {'Freddie Mercury': 'e8_t9_p8_s8',
          'Paul McCartney': 'e8_t7_p7_s8',
          'Klaus Meine': 'e9_t8_p9_s8'
 }
+
