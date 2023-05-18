c0898a7a8760fc29b8a5f60c59c27f7681f9d5e7
Cristhian Monteiro <cristhiansousa8@gmail.com>
2023-05-18 00:05:12 -0300

    Inserido um While, Identação.
    Incrementado uma estrutura de repetição While para perguntar ao usuário se deseja fazer uma nova pesquisa após apresentar os resultados de uma busca.
    A identação foi ajustada.

--- resilia_pi_m2_selecao_candidatos_entrevista.py
+++ resilia_pi_m2_selecao_candidatos_entrevista.py
@@ -7,35 +7,41 @@ def buscar_candidatos(resultados, nota_entrevista, nota_teorico, nota_pratica, n
             candidatos.append(candidato)
 
     return candidatos
+while True:
 
-#Entrada das notas mínimas informadas pelo usuario
-nota_entrevista = int(input("Digite a nota mínima (de 0 a 9) desejada na entrevista: "))
-nota_teorico = int(input("Digite a nota mínima (de 0 a 9) desejada no teste teórico: "))
-nota_pratica = int(input("Digite a nota mínima (de 0 a 9) desejada no teste prático: "))
-nota_soft = int(input("Digite a nota mínima (de 0 a 9) desejada na avaliação de soft skills: "))
+    #Entrada das notas mínimas informadas pelo usuario
+    nota_entrevista = int(input("Digite a nota mínima (de 0 a 9) desejada na Entrevista: "))
+    nota_teorico = int(input("Digite a nota mínima (de 0 a 9) desejada no Teste Teórico: "))
+    nota_pratica = int(input("Digite a nota mínima (de 0 a 9) desejada no Teste Prático: "))
+    nota_soft = int(input("Digite a nota mínima (de 0 a 9) desejada na avaliação de Soft Skills: "))
 
-#Usuarios e notas disponiveis no codigo
-resultados = {'Freddie Mercury': 'e8_t9_p8_s8',
+    #Usuarios e notas disponiveis no codigo
+    resultados = {'Freddie Mercury': 'e8_t9_p8_s8',
          'Roger Waters': 'e8_t8_p7_s7',
          'Kurt Cobain': 'e5_t7_p6_s3',
          'Paul McCartney': 'e8_t7_p7_s8',
          'Klaus Meine': 'e8_t8_p9_s8'
-}
+    }
 
-#Função que checa os dados fornecidos pela função "busca_candidatos" e mostra o resultado
-#Também faz a classificação por ordem alfabetica atraves do 'sorted' e retorna uma mensagem caso não encontre candidato.
-candidatos_entrevistados = buscar_candidatos(resultados, nota_entrevista, nota_teorico, nota_pratica, nota_soft)
-if len(candidatos_entrevistados) == 0:
-    print('\n'+'=-'*36)
-    print('\033[33m''\033[1m''Não foi possível encontrar candidatos com os parâmetros que você definiu.''\033[0m')
-    print('=-'*36,'\n')
+    #Função que checa os dados fornecidos pela função "busca_candidatos" e mostra o resultado
+    #Também faz a classificação por ordem alfabetica atraves do 'sorted' e retorna uma mensagem caso não encontre candidato.
+    candidatos_entrevistados = buscar_candidatos(resultados, nota_entrevista, nota_teorico, nota_pratica, nota_soft)
+    if len(candidatos_entrevistados) == 0:
+        print('\n'+'=-'*36)
+        print('\033[33m''\033[1m''Não consegui encontrar candidatos com os parâmetros que me solicitou.''\033[0m')
+        print('=-'*36)
 
-else:
-    print("\033[1;36m"+"Excelente!"+"\033[0;0m",'Excelente! Encontrei os(as) seguintes candidatos(as):')
-    candidatos_entrevistados = sorted(candidatos_entrevistados)
-    for candidato in candidatos_entrevistados:
-        nota = resultados[candidato]
-        print('=-'*31)
-        print("\033[32m{}: {}\033[0;0m \n".format(candidato,nota))
+    else:
+        print("\n"+"\033[1;36m"+"Excelente!"+"\033[0;0m",'Excelente! Encontrei os(as) seguintes candidatos(as):')
+        candidatos_entrevistados = sorted(candidatos_entrevistados)
+        for candidato in candidatos_entrevistados:
+            nota = resultados[candidato]
+            print('=-'*31)
+            print("\033[32m{}: {}\033[0;0m".format(candidato,nota))
 
-print('FIM=-=-=-=-=-=-=-=-=-=-FIM=-=-=-=-=-=-=-=-=-=-'*4)
+
+    print('FIM=-=-=-=-=-=-=-=-=-=-FIM=-=-=-=-=-=-=-=-=-=-'*4)
+
+    resposta = input('Deseja fazer uma nova busca? (sim,não)')
+    if resposta.lower() not in ['sim','s']:
+        break
